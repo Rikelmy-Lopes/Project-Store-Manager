d65448d3810cd867ca81971cae3c3d484a07a5e9
Rikelmy Lopes <rikelmylopes899@gmail.com>
2022-10-12 12:33:43 -0300

    .

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use('/', router);
 
-// não remova essa exportação, é para o avaliador funcionar.
+// não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;
